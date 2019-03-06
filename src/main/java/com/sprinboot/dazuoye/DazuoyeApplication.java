028ac30c2da67b1d7e92a018cd9c4e9193caf236
linqiyuan <466214222@qq.com>
2019-03-06 19:45:15 +0800

    Ver1.0

---
+++ src/main/java/com/sprinboot/dazuoye/DazuoyeApplication.java
@@ -0,0 +1,13 @@
+package com.sprinboot.dazuoye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DazuoyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DazuoyeApplication.class, args);
+    }
+
+}
