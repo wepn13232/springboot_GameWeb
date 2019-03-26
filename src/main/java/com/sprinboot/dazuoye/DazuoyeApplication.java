a1eb6cef6e279df4ea46f353b0e366e7209fbb3d
linqiyuan <466214222@qq.com>
2019-03-27 07:46:42 +0800

    Ver2.3 增加前端

--- src/main/java/com/sprinboot/dazuoye/DazuoyeApplication.java
+++ src/main/java/com/sprinboot/dazuoye/DazuoyeApplication.java
@@ -2,9 +2,10 @@ package com.sprinboot.dazuoye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DazuoyeApplication {
+public class DazuoyeApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DazuoyeApplication.class, args);
