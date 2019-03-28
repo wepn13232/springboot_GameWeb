05b860a61ee13383dd97a86240ca70e9baed6b52
linqiyuan <466214222@qq.com>
2019-03-28 23:52:03 +0800

    Ver2.7

--- src/main/java/com/sprinboot/dazuoye/dao/GameMapper.java
+++ src/main/java/com/sprinboot/dazuoye/dao/GameMapper.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.HashMap;
 import java.util.List;
 
-@Repository
+@Repository("gameMapper")
 @Mapper
 public interface GameMapper {
 
