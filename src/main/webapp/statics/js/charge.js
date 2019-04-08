cf4abfac726bc340b81d7d0e029dfdc323d5ba64
wepn13232a <lqy131898a>
2019-04-08 13:00:12 +0800

    Ver 3.2.3 修复充值bug

--- src/main/webapp/statics/js/charge.js
+++ src/main/webapp/statics/js/charge.js
@@ -2,7 +2,7 @@ $(function () {
     $(".charge_btn1").bind("click", showCash);
     $(".charge_btn2").bind("click", showCash);
     $(".charge_btn3").bind("click", showCash);
-    $("#selfCash").bind("blur", showCash);
+    $("#selfCash").bind("blur", showCash1);
     $("#charge_button").bind("click", confirm); //付款按钮
     $(".wcmodalBtn").bind("click", wcLocationHref) //完成付款按钮
 });
@@ -12,8 +12,13 @@ function showCash() {
     var cashNum = $(this).val();
     // alert(cashNum);
     $('.showCashNum').html(~~cashNum);
-    $("#selfCash").val(~~cashNum);
     // alert(num.html());
+}
+function showCash1() {
+    var cashNum = $(this).val();
+    $('.showCashNum').html(~~cashNum);
+    $("#selfCash").val(~~cashNum);
+
 }
 
 
