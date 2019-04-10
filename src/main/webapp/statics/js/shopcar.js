9dc68bd48d98f0c6353bd23d3672092592fc0a0c
linqiyuan <466214222@qq.com>
2019-04-10 19:42:33 +0800

    修改一点前端样式

--- src/main/webapp/statics/js/shopcar.js
+++ src/main/webapp/statics/js/shopcar.js
@@ -38,6 +38,42 @@ function buyGame() {
     $("#payModal").modal("show");
 }
 
+//付款操作模态框（输入密码）
+function confirmBuyGame() {
+    if($("#payModal").modal("hide")){
+        //输入密码，判断密码
+        if(AJAX){
+            $.ajax({
+                contentType: "application/json; charset=utf-8",
+                type: "get",
+                url: "/buygame",
+                data: {
+                    "id": $("input[data-id=" + id + "]").val(), "game_price": $("input[data-gameprice=" + id + "]").val()
+                },
+                async: true,
+                dataType: "json",
+                success: function (data) {
+                    if (data.msg === "success") {
+                        window.location.reload();
+                    }
+                    if (data.msg === "error") {
+                        alert("付款失败");
+                    }
+                    if (data.msg === "less") {
+                        window.confirm("余额不足，是否现在充值？");
+                        window.location.href = "/user/charge?username=" + data.username;
+                    }
+                },
+                error: function () {
+
+                }
+            })
+        }else{
+
+        }
+    }
+}
+
 //删除订单
 function deleteGame() {
     var id = $(this).attr('id');
